9a6651d82973e922a3b4b0b6a8d8f181674aa2c7
HUNMrsen <hunmrsen@163.com>
2024-12-04 23:04:27 +0800

    update the topo generate scripts

--- astra-sim-alibabacloud/inputs/topo/gen_HPN_7.0_topo_mulgpus_one_link.py
+++ astra-sim-alibabacloud/inputs/topo/gen_HPN_7.0_topo_mulgpus_one_link.py
@@ -16,7 +16,7 @@ def gen_HPN_7_0_single_plane(args):
     # for verification
     gpu_to_links = {}
 
-    file_name = "HPN_7_0_"+str(args.gpu)+"_gpus_"+str(args.gpu_per_server)+"_in_one_server_with_"+args.bandwidth+"_"+args.gpu_type
+    file_name = "HPN_7_0_"+str(args.gpu)+"_gpus_"+str(args.gpu_per_server)+"_in_one_server_with_single_plane_"+args.bandwidth+"_"+args.gpu_type
     with open(file_name, 'w') as f:
         print(file_name)
         first_line = str(nodes)+" "+str(args.gpu_per_server)+" "+str(nv_switch_num)+" "+str(switch_nodes-nv_switch_num)+" "+str(int(links))+" "+str(args.gpu_type)
@@ -84,7 +84,7 @@ def gen_HPN_7_0_multi_plane(args):
     # for verification
     gpu_to_links = {}
 
-    file_name = "HPN_7_0_"+str(args.gpu)+"_gpus_"+str(args.gpu_per_server)+"_in_one_server_with_"+args.bandwidth+"_"+args.gpu_type
+    file_name = "HPN_7_0_"+str(args.gpu)+"_gpus_"+str(args.gpu_per_server)+"_in_one_server_with_multi_plane"+args.bandwidth+"_"+args.gpu_type
     with open(file_name, 'w') as f:
         print(file_name)
         first_line = str(nodes)+" "+str(args.gpu_per_server)+" "+str(nv_switch_num)+" "+str(switch_nodes-nv_switch_num)+" "+str(int(links))+" "+str(args.gpu_type)
@@ -164,10 +164,80 @@ def gen_HPN_7_0_multi_plane(args):
                 f.write(line)
                 f.write('\n')
 
+def gen_HPN_6_2_single_plane(args):
+    asw_switch_num = args.asw_switch_num
+    nv_switch_num = (int)(args.gpu / args.gpu_per_server) * args.nv_switch_per_server
+    nodes_per_2asw = args.nics_per_aswitch # 每个asw下行连接数
+    nodes = (int) (args.gpu + asw_switch_num + args.psw_switch_num + nv_switch_num) # 
+    servers = args.gpu / args.gpu_per_server
+    switch_nodes = (int)(args.psw_switch_num + asw_switch_num + nv_switch_num) # 
+    links = (int)((args.psw_switch_num / 2) * asw_switch_num + servers * args.gpu_per_server+ servers * args.nv_switch_per_server * args.gpu_per_server) # 
+
+    # for verification
+    gpu_to_links = {}
+    file_name = "HPN_6_2_"+str(args.gpu)+"_gpus_"+str(args.gpu_per_server)+"_in_one_server_with_single_plane_"+args.bandwidth+"_"+args.gpu_type
+
+    with open(file_name, 'w') as f:
+        print(file_name)
+        first_line = str(nodes)+" "+str(args.gpu_per_server)+" "+str(nv_switch_num)+" "+str(switch_nodes-nv_switch_num)+" "+str(int(links))+" "+str(args.gpu_type)
+        f.write(first_line)
+        f.write('\n')
+        nv_switch = []
+        asw_switch_1 = []
+        psw_switch_1 = []
+        psw_switch_2 = []
+        sec_line = ""
+        nnodes = nodes - switch_nodes
+        for i in range(nnodes, nodes):
+            sec_line = sec_line + str(i) + " "
+            if len(nv_switch) < nv_switch_num:
+                nv_switch.append(i)
+            elif len(asw_switch_1) < asw_switch_num:
+                asw_switch_1.append(i)
+            elif len(psw_switch_1) < args.psw_switch_num / 2:
+                psw_switch_1.append(i)
+            else:
+                psw_switch_2.append(i)
+        f.write(sec_line)
+        f.write('\n')
+        
+        ind_asw1 = 0
+        curr_node = 0
+        ind_nv = 0
+        asw_node = 0
+        cnt = 0
+        for i in range(args.gpu):
+            curr_node = curr_node + 1
+            asw_node = asw_node + 1
+            if curr_node > args.gpu_per_server:
+                curr_node = 1
+                ind_nv = ind_nv + args.nv_switch_per_server
+            
+            for j in range(0, args.nv_switch_per_server):
+                cnt += 1
+                line = str(i)+" "+str(nv_switch[ind_nv+j])+" "+args.nvlink_bw+" "+args.nv_latency+" "+args.error_rate
+                f.write(line)
+                f.write('\n')
+
+            if asw_node > nodes_per_2asw:
+                asw_node = 1
+                ind_asw1 = ind_asw1 + 1
+
+            line = str(i)+" "+str(asw_switch_1[ind_asw1])+" "+args.bandwidth+" "+args.latency+" "+args.error_rate
+            f.write(line)
+            f.write('\n')
+            
+        # double logical plane
+        for i in asw_switch_1: # asw - psw
+            for j in psw_switch_1:
+                line = str(i) + " " + str(j) +" "+ args.ap_bandwidth+" " + args.latency+" " +args.error_rate
+                f.write(line)
+                f.write('\n')
+
 def gen_HPN_6_2_multi_plane(args):
     asw_switch_num = args.asw_switch_num
     nv_switch_num = (int)(args.gpu / args.gpu_per_server) * args.nv_switch_per_server
-    nodes_per_2asw = 32 # 每个asw下行连接数
+    nodes_per_2asw = args.nics_per_aswitch # 每个asw下行连接数
     nodes = (int) (args.gpu + asw_switch_num + args.psw_switch_num + nv_switch_num) # 
     servers = args.gpu / args.gpu_per_server
     switch_nodes = (int)(args.psw_switch_num + asw_switch_num + nv_switch_num) # 
@@ -175,7 +245,7 @@ def gen_HPN_6_2_multi_plane(args):
     # for verification
     gpu_to_links = {}
 
-    file_name = "HPN_6_2_"+str(args.gpu)+"_gpus_"+str(args.gpu_per_server)+"_in_one_server_with_"+args.bandwidth+"_"+args.gpu_type
+    file_name = "HPN_6_2_"+str(args.gpu)+"_gpus_"+str(args.gpu_per_server)+"_in_one_server_with_multi_plane"+args.bandwidth+"_"+args.gpu_type
     with open(file_name, 'w') as f:
         print(file_name)
         first_line = str(nodes)+" "+str(args.gpu_per_server)+" "+str(nv_switch_num)+" "+str(switch_nodes-nv_switch_num)+" "+str(int(links))+" "+str(args.gpu_type)
@@ -267,11 +337,15 @@ def main():
     parser.add_argument('-psn','--psw_switch_num',type=int,default=120,help='psw_switch_num,default 120')
     parser.add_argument('-asn','--asw_switch_num',type=int,default=64,help='asw_switch_num,default 64')
     parser.add_argument('-nsps','--nv_switch_per_server',type=int,default=1,help='nv_switch_per_server,default 1')
+    parser.add_argument('-npa','--nics_per_aswitch',type=int,default=128,help='nnics per asw,default 128')
     parser.add_argument('--dp', action='store_true', help='enable dual_plane, default single plane')
     parser.add_argument('--st', action='store_true', help='enable HPN_6_2 architecture , default HPN_7_0')
     args = parser.parse_args()
     if args.st:
-        gen_HPN_6_2_multi_plane(args)
+        if args.dp:
+            gen_HPN_6_2_multi_plane(args)
+        else:
+            gen_HPN_6_2_single_plane(args)
     else:
         if args.dp:
             gen_HPN_7_0_multi_plane(args)
