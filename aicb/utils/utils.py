bbcbd6313137bf3887438a29025be3189ae7e46c
yanzhenghao <yanzhenghao2@huawei.com>
2026-06-15 19:05:24 +0800

    fix: escape docstring backslashes in generate_masked_orthogonal_rank_groups
    \i and \d were interpreted as invalid escape sequences in Python 3.14.
    Changed to raw string (r""").

--- aicb/utils/utils.py
+++ aicb/utils/utils.py
@@ -34,7 +34,7 @@ except ImportError as e:
 def generate_masked_orthogonal_rank_groups(
     world_size: int, parallel_size: List[int], mask: List[bool],
 ) -> List[List[int]]:
-    """Generate orthogonal parallel groups based on the parallel size and mask.
+    r"""Generate orthogonal parallel groups based on the parallel size and mask.
 
     Arguments:
         world_size (int): world size
